0b2f0fc9521387eea707d26146eeb155ae3e31d6
Your Name <your.email@example.com>
2025-03-16 14:38:31 -0400

    Task 2 - Adding Risk Items

--- cc15.js
+++ cc15.js
@@ -1,5 +1,8 @@
 // Task 2: Adding Risk Items Dynamically
 
+
+const riskDashboard = document.getElementById("riskDashboard");
+
 // creating a function to add risk items
 function addRiskItem(riskName, riskLevel, department) {
 // make a risk card
@@ -12,9 +15,18 @@ riskCard.innerHTML = `
         <h3>${riskName}</h3>
         <p>Risk Level: ${riskLevel}</p>
         <p>Department: ${department}</p>
-        <button class="resolve-btn">Resolve</button>
     `;
 
+riskDashboard.appendChild(riskCard);
+
+
+document.getElementById('submitButton').addEventListener('click', () => {
+    
+    const riskName = document.getElementById('riskName').value;
+    const riskLevel = document.getElementById('riskLevel').value;
+    const department = document.getElementById('department').value;
+
+});
 
 }
 
